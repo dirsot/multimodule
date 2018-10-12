04a840a97cb787274d8b28e1007fd545ac1de8b9
Lukasz Zienkowski <ext-lukasz.zienkowski@finago.com>
2018-10-13 01:23:06 +0200

    3

--- application/src/main/java/hello/app/DemoApplication.java
+++ application/src/main/java/hello/app/DemoApplication.java
@@ -4,12 +4,15 @@ import hello.service.MyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"hello.app", "hello.service"})
 @RestController
 public class DemoApplication {
 
+    //@Autowired
     private final MyService myService;
 
     public DemoApplication(MyService myService) {
@@ -21,7 +24,7 @@ public class DemoApplication {
         return myService.message();
     }
 
-    @GetMapping("/test")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test() {
         return "test";
     }
